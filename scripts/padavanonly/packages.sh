a09743f65ac2bc409905b349311236c133e1e886
zxg <zhouleike@live.com>
2025-12-07 00:07:15 +0800

    项目调整

--- scripts/padavanonly/packages.sh
+++ scripts/padavanonly/packages.sh
@@ -49,15 +49,6 @@ UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-24.10"
 ##UPDATE_PACKAGE "aurora" "eamonxg/luci-theme-aurora" "master"
 ##UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "js"
 
-##UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
-##UPDATE_PACKAGE "momo" "nikkinikki-org/OpenWrt-momo" "main"
-##UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
-##UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
-##UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
-##UPDATE_PACKAGE "passwall2" "xiaorouji/openwrt-passwall2" "main" "pkg"
-
-##UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
-
 UPDATE_PACKAGE "ddns-go" "sirpdboy/luci-app-ddns-go" "main"
 UPDATE_PACKAGE "diskman" "lisaac/luci-app-diskman" "master"
 UPDATE_PACKAGE "fancontrol" "rockjake/luci-app-fancontrol" "main"
