4b4fea30a26768fab8ad9214376199b1b1f30d9a
zxg <zhouleike@live.com>
2025-12-15 20:44:27 +0800

    修改完善工作流文件和sh脚本文件，重新组织相关配置文件，为加入OpenWRT后的自动构建作好代码复用的准备。

--- scripts/padavanonly/settings.sh
+++ scripts/padavanonly/settings.sh
@@ -8,35 +8,35 @@
 #移除luci-app-attendedsysupgrade
 sed -i "/attendedsysupgrade/d" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 #修改默认主题
-sed -i "s/luci-theme-bootstrap/luci-theme-$PADAVANONLY_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
+sed -i "s/luci-theme-bootstrap/luci-theme-$OC_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 #修改immortalwrt.lan关联IP
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/$PADAVANONLY_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/$OC_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 #添加编译日期标识
-sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $PADAVANONLY_MARK-$PADAVANONLY_DATE')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
+sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $OC_MARK-$OC_DATE')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
 
 WIFI_FILE="./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh"
 #修改WIFI名称
-sed -i "s/ImmortalWrt/$PADAVANONLY_SSID/g" $WIFI_FILE
+sed -i "s/ImmortalWrt/$OC_SSID/g" $WIFI_FILE
 #修改WIFI加密
 sed -i "s/encryption=.*/encryption='psk2+ccmp'/g" $WIFI_FILE
 #修改WIFI密码
-sed -i "/set wireless.default_\${dev}.encryption='psk2+ccmp'/a \\\t\t\t\t\t\set wireless.default_\${dev}.key='$PADAVANONLY_WORD'" $WIFI_FILE
+sed -i "/set wireless.default_\${dev}.encryption='psk2+ccmp'/a \\\t\t\t\t\t\set wireless.default_\${dev}.key='$OC_WORD'" $WIFI_FILE
 
 CFG_FILE="./package/base-files/files/bin/config_generate"
 #修改默认IP地址
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/$PADAVANONLY_IP/g" $CFG_FILE
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/$OC_IP/g" $CFG_FILE
 #修改默认主机名
-sed -i "s/hostname='.*'/hostname='$PADAVANONLY_NAME'/g" $CFG_FILE
+sed -i "s/hostname='.*'/hostname='$OC_NAME'/g" $CFG_FILE
 
 #配置文件修改
 echo "CONFIG_PACKAGE_luci=y" >> ./.config
 echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
-echo "CONFIG_PACKAGE_luci-theme-$PADAVANONLY_THEME=y" >> ./.config
-echo "CONFIG_PACKAGE_luci-app-$PADAVANONLY_THEME-config=y" >> ./.config
+echo "CONFIG_PACKAGE_luci-theme-$OC_THEME=y" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-$OC_THEME-config=y" >> ./.config
 
 #手动调整的插件
-if [ -n "$PADAVANONLY_PACKAGE" ]; then
-    echo -e "$PADAVANONLY_PACKAGE" >> ./.config
+if [ -n "$OC_PACKAGE" ]; then
+    echo -e "$OC_PACKAGE" >> ./.config
 fi
 
 # 添加防火墙规则
